67daf5863704a282d0e3e6d03899fc31c354fa01
xiayulu <harolddfinchh@gmail.com>
2022-01-25 08:36:26 +0800

    multi thread

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        Self { workers, sender }
+    }
+
+    pub fn excute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(move || {
+            while let Ok(job) = receiver.lock().unwrap().recv() {
+                println!("Worker {} got a job; executing.", id);
+                job();
+            }
+        });
+        Self { id, thread }
+    }
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,21 @@ use std::{
     fs,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+
+use pong::ThreadPool;
 
 fn main() {
     let server = TcpListener::bind("127.0.0.1:9090").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in server.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        pool.excute(|| {
+            handle_connection(stream);
+        });
         println!("Connection established!");
     }
 }
@@ -16,9 +24,13 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
     stream.read(&mut buffer).unwrap();
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(2));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
